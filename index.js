9f8a340d9d024eb055c83fbcb98466c27ac85dba
Andrew <andrewchen796@gmail.com>
2026-03-11 21:54:31 +0000

    Add basic table

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
+const gameNames = new Map([
+  [10,     'Counter-Strike'],
+  [20,     'Team Fortress Classic'],
+  [30,     'Day of Defeat'],
+  [40,     'Deathmatch Classic'],
+  [50,     'Opposing Force'],
+  [60,     'Ricochet'],
+  [70,     'Half-Life'],
+  [80,     'Condition Zero'],
+  [240,    'Counter-Strike: Source'],
+  [300,    'Day of Defeat: Source'],
+  [320,    'Half-Life 2: Deathmatch'],
+  [360,    'Half-Life Deathmatch: Source'],
+  [440,    'Team Fortress 2'],
+  [500,    'Left 4 Dead'],
+  [550,    'Left 4 Dead 2'],
+  [570,    'Dota 2'],
+  [620,    'Portal 2'],
+  [630,    'Alien Swarm'],
+  [730,    'Counter-Strike: Global Offensive'],
+  [1800,   'Counter-Strike 2'],
+  [1300,   'SiN Episodes: Emergence'],
+  [2130,   'Dark Messiah of Might and Magic Multi-Player'],
+  [2400,   'The Ship: Murder Party'],
+  [2450,   'Bloody Good Time'],
+  [4000,   'Garry\'s Mod'],
+  [17500,  'Zombie Panic! Source'],
+  [17510,  'Age of Chivalry'],
+  [17520,  'Synergy'],
+  [17530,  'D.I.P.R.I.P.'],
+  [17550,  'Eternal Silence'],
+  [17570,  'Pirates, Vikings, & Knights II'],
+  [17580,  'Dystopia'],
+  [17700,  'Insurgency'],
+  [17710,  'Nuclear Dawn'],
+  [17730,  'Smashball'],
+  [222880, 'Insurgency (2014)'],
+  [224260, 'No More Room in Hell'],
+  [238430, 'Contagion'],
+]);
+
 const WORKER_URL = 'https://steam-game-servers.andrewchen796.workers.dev?';
 
 async function getServers(limit, filter) {
@@ -17,3 +58,40 @@ async function getServers(limit, filter) {
     return null;
   }
 }
+
+function transformData(data) {
+  if (!data) return [];
+
+  let servers = [];
+
+  data.forEach((server) => {
+    servers.push([
+      server.name ?? 'Server Name Not Found',
+      server.addr ?? 'Server IP Not Found',
+      gameNames.get(server.appid) ?? 'Game Not Found',
+      server.map ?? 'Map Not Found',
+      (server.players ?? 0) + '/' + (server.max_players ?? '?'),
+      server.secure ? '✅' : '❌',
+    ]);
+  });
+
+  return servers;
+}
+
+function createTable(data) {
+  return new gridjs.Grid({
+    columns: ['Server Name', 'Server IP', 'Game', 'Map', 'Players', 'Secure'],
+    sort: true,
+    search: {
+      selector: (cell, rowIndex, cellIndex) => cellIndex === 0 ? cell : null
+    },
+    pagination: {
+      limit: 15,
+    },
+    data: () => {
+      return data.then((data) => transformData(data.servers));
+    },
+  });
+}
+
+createTable(getServers()).render(document.getElementById('wrapper'));
