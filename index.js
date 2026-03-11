a25c40fde3bdf4b7aaa1595f094bb26d2274197f
Andrew <andrewchen796@gmail.com>
2026-03-11 17:56:18 +0000

    Add function to fetch data from worker

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Hello, World!')
+const WORKER_URL = 'https://steam-game-servers.andrewchen796.workers.dev?';
+
+async function getServers(limit, filter) {
+  const url = new URL(WORKER_URL);
+  if (limit) url.searchParams.set('limit', limit);
+  if (filter) url.searchParams.set('filter', filter);
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      return null;
+    }
+
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    return null;
+  }
+}
